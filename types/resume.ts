50eda643abcb50baf442c903597aabd3fa55abdf
mfsi.sudhanshu <mfsi.sudhanshuk>
2025-01-12 03:28:17 +0530

    type added

---
+++ types/resume.ts
@@ -0,0 +1,50 @@
+import { ReactNode } from 'react'
+
+export interface Theme {
+  background: string
+  text: string
+  primary: string
+  secondary: string
+  button: string
+  buttonRipple: string
+  iconBackground: string
+  icon: string
+  card: string
+  skillCard: string
+  activityCard: string
+  header: string
+  headerLink: string
+  timeline: {
+    line: string
+    dot: string
+  }
+  timelineAnimation: string
+  heroPattern: string
+  floatingIcons: string
+}
+
+export interface Job {
+  title: string
+  company: string
+  year: string
+  description: string[] // Ensure this is always an array of strings
+}
+
+export interface Service {
+  icon: ReactNode
+  title: string
+  description: string
+}
+
+export interface Skill {
+  category: string
+  icon: ReactNode
+  items: string[]
+}
+
+export interface Activity {
+  icon: ReactNode
+  title: string
+  description: string
+}
+
